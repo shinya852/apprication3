a18906297befb528772e4e7ce4762ad7b5d88555
shinya852 <shinya.june@gmail.com>
2021-10-06 04:08:53 +0000

    [Update]DB&Contoroller

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,27 @@
+class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+  @book = Book.find(params[:book_id])
+  @book_comment = @book.book_comments.new(book_comment_params)
+  @book_comment.user_id = current_user.id
+    if @book_comment.save
+    redirect_to request.referer, notice: 'successfully created'
+    else
+    redirect_to book_path(@book)
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find(params[:book_id])
+    @book_comment.destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,2 +1,7 @@
 class BookComment < ApplicationRecord
+  belongs_to :user
+  belongs_to :book
+
+  validates :comment, presence: true, length: { maximum: 140}
+
 end
