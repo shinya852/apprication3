8bd6061d1564df84ecfdbdf94d2a2f3de41498da
shinya852 <shinya.june@gmail.com>
2021-10-06 03:06:13 +0000

    start comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_102147) do
+ActiveRecord::Schema.define(version: 2021_10_06_025554) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.string "comment"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_book_comments_on_book_id"
+    t.index ["user_id"], name: "index_book_comments_on_user_id"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
